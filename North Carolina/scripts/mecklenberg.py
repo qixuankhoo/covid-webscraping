72577fccc44bbb78e4344729365e54e9b0e958b1
Rami Sbahi <sbahirami@gmail.com>
2020-08-03 10:33:42 -0400

    meck

--- North Carolina/scripts/mecklenberg.py
+++ North Carolina/scripts/mecklenberg.py
@@ -28,13 +28,11 @@ def getPDF(file_url, county):
     title = ' '.join(file_url.split('/')).split().pop()
     if '.pdf' not in title:
         title += '.pdf'
-    print("file_url", file_url)
-    print(file_url.split("/"))
     fileName = title
-    print("title" ,title)
-    filePath = getFilePath("../data/" + county + "-PDF")
+    filePath = "../data/" + county + "-PDF"
     r = requests.get(file_url, stream = True)
-    with open(os.path.join(filePath,fileName), "wb") as pdf:
+    print("the path", filePath + "/" + fileName)
+    with open(filePath + "/" + fileName, "wb") as pdf:
         for chunk in r.iter_content(chunk_size=1024):
          if chunk:
              pdf.write(chunk)
@@ -78,10 +76,10 @@ for section in sections:
             try:
                 full_url = link
                 if 'mecknc.gov' not in full_url:
-                    full_url = 'https://www.mecknc.gov/' + full_url
+                    full_url = 'https://www.mecknc.gov' + full_url
                 currSoup = scraping(full_url)
                 f.write(currSoup.select('.mc-page-content')[0].get_text())
             except:
-                pdf = getPDF('https://www.mecknc.gov/'+link, COUNTY)
+                getPDF('https://www.mecknc.gov/'+link, COUNTY)
 
 f.close()
