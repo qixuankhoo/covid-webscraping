9e53a3d2a815910ead0cec9aedc5793d9c68882f
Anni Chen <anni.chen@duke.edu>
2020-08-04 17:28:03 -0400

    Run all script from anni

--- Colorado/scripts/run_all.py
+++ Colorado/scripts/run_all.py
@@ -24,7 +24,7 @@ for county in county_list:
         print(county + " is not available")
         pass
 # move all the pdfs
-pdf_list = ["boulder-PDF", "tri-county-PDF", "weld-PDF"]
+pdf_list = ["boulder-PDF", "tri-county-PDF", "weld-PDF", "el_paso-PDF", "larimer-PDF"]
 for folder in pdf_list:
     try:
         shutil.move("../data/" + folder, "../data/" + today + "/" + folder)
