ab72e387ad37a145abcc0126e9a26b4185b30998
Rami Sbahi <sbahirami@gmail.com>
2020-08-11 13:01:10 -0400

    baton rouge

--- Louisiana/scripts/east_baton_rouge.py
+++ Louisiana/scripts/east_baton_rouge.py
@@ -54,9 +54,11 @@ for i in sublinks:
 
 PDFS = []
 for i in sublinks:
+    print("idk")
+    print(i)
     if "Word" in i:
         continue
-    if ("Center/View" in i and "brla.gov" in i) or ".pdf" in i:
+    if ("Center/View" in i and "brla.gov" in i) or ".pdf" in i or "brla.gov/businessguidelines" in i:
         PDFS.append(i)
     elif "brla.gov" in i:
         soup = scraping(i)
@@ -64,13 +66,9 @@ for i in sublinks:
         for y in range(len(data)):
             f.write(data[y].get_text(separator = '\n'))
 
-
-import PyPDF2
-import io
 import glob
 
 import requests
-from PyPDF2 import PdfFileReader
 
 path = "../data/" + COUNTY + "-PDF"
 fileDir = os.path.dirname(__file__)
@@ -80,32 +78,16 @@ os.makedirs(filePath, exist_ok=True)
 
 def getPDFs(file_url, county):
     title = file_url.split('/').pop()
+    if ".pdf" not in title:
+        title += ".pdf"
     r = requests.get(file_url, stream = True)
-    path = "../data/" + COUNTY + "-PDF" + "/" + title
-    fileDir = os.path.dirname(__file__)
-    filePath = os.path.join(fileDir, path)
-    filePath = os.path.abspath(os.path.realpath(filePath))
-    h = open(filePath, "wb")
-    with h as pdf:
-        for chunk in r.iter_content(chunk_size=1024):
-         if chunk:
-             pdf.write(chunk)
-    return "data/" + title
+    with open("../data/" + county + "-PDF" + "/" + title,"wb") as pdf:
+        for chunk in r.iter_content(chunk_size=1024*1024):
+            pdf.write(chunk)
 
-print(PDFS)
 
 for url in PDFS:
     getPDFs(url, COUNTY)
-    print("Scraping from" + url)
-    r = requests.get(url)
-    fi = io.BytesIO(r.content)
-    reader = PdfFileReader(fi)
-    number_of_pages = reader.getNumPages()
-    for page_number in range(number_of_pages):
-        page = reader.getPage(page_number)
-        page_content = page.extractText()
-        f.write(page_content)
-
-f.close()
+    
 driver.quit()
 print("finished")
