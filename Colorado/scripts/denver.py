4b00030a40b15cb51cb31f9e856deaa5faeb23cd
Rami Sbahi <sbahirami@gmail.com>
2020-08-14 10:06:22 -0400

    denver updated

--- Colorado/scripts/denver.py
+++ Colorado/scripts/denver.py
@@ -17,6 +17,17 @@ def findHref(data):
         for link in data[i].find_all('a', class_='btn-info'):
             links.append(link.get('href'))
 
+def getPDFs(file_url, county):
+    print("A PDF HERE\n")
+    f.write("A PDF HERE\n")
+    title = file_url.split('/').pop()
+    if '.pdf' not in title:
+        title += '.pdf'
+    r = requests.get(file_url, stream = True)
+    with open("../data/" + county + "-PDF" + "/" + title,"wb") as pdf:
+        for chunk in r.iter_content(chunk_size=1024*1024):
+            pdf.write(chunk)
+
 
 #create PDF folder for PDF files
 COUNTY = 'denver'
@@ -35,6 +46,14 @@ links = []
 url = 'https://www.denvergov.org/content/denvergov/en/covid-19/recovery-guidance.html'
 webpage = requests.get(url)
 soup = BeautifulSoup(webpage.content, 'html.parser')
+
+body = soup.select_one('.denver-panel.section .denver-panel-body')
+for a_tag in body.find_all('a'):
+    print(a_tag.text)
+    pdf_link = a_tag.get('href')
+    if 'http' not in pdf_link:
+        pdf_link = 'https://www.denvergov.org' + pdf_link
+    getPDFs(pdf_link, 'denver')
 links = []
 data = soup.find_all("div", class_="rawtext section")
 findHref(data)
